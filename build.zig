1ec0a79289bf778dfefb46559a709b1fa209bb9c
gnasc <gabrielnsdev@gmail.com>
2025-12-02 21:04:34 -0300

    second puzzle

--- build.zig
+++ build.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const exe = b.addExecutable(.{
-        .name = "hello",
+        .name = "day01",
         .root_module = b.createModule(.{
             .root_source_file = b.path("src/day01.zig"),
             .target = b.graph.host,
--- src/day01.zig
+++ src/day01.zig
@@ -1,20 +1,29 @@
 const std = @import("std");
 
-const Dial = struct{
+const Dial = struct {
     position: u8 = 0,
+    count: usize = 0,
 
     fn moveLeft(self: *Dial, amount: usize) void {
+        var cycles = amount / 100;
         const amount_u8: u8 = @as(u8, @intCast(amount % 100));
-    
+
+        if(self.position != 0 and self.position <= amount_u8) cycles += 1;
         self.position -%= amount_u8;
+
         self.position %= 156;
+        self.count += cycles;
     }
 
     fn moveRight(self: *Dial, amount: usize) void {
+        var cycles = @as(u8, @intCast(amount / 100));
         const amount_u8: u8 = @as(u8, @intCast(amount % 100));
 
         self.position +%= amount_u8;
+        if(self.position % 100 != self.position) cycles += 1;
+
         self.position %= 100;
+        self.count += cycles;
     }
 };
 
@@ -25,27 +34,25 @@ pub fn main() !void {
     var reader_buffer: [4096]u8 = undefined;
     var data_file = try std.fs.openFileAbsolute(path, .{});
     var data_reader = data_file.reader(&reader_buffer);
-    
+
     const reader: *std.Io.Reader = &data_reader.interface;
 
     var dial: Dial = Dial{ .position = 50 };
-    var count: usize = 0;
 
-    while(true) {
+    while (true) {
         const line = reader.takeDelimiterExclusive('\n') catch break;
         const number: usize = try std.fmt.parseUnsigned(usize, line[1..], 10);
 
-        switch(line[0]) {
+        switch (line[0]) {
             'L' => dial.moveLeft(number),
             'R' => dial.moveRight(number),
             else => unreachable,
         }
-        
-        if(dial.position == 0) count += 1;
 
-//        std.debug.print("The dial is rotated {s} to point at {d}\n", .{line, dial.position});
+        //std.debug.print("Move: {s} Position: {d} Count: {d}\n", .{line, dial.position, dial.count});
+
         reader.toss(1);
     }
 
-    std.debug.print("Password is: {d}\n", .{count});
+    std.debug.print("Password is: {d}\n", .{dial.count});
 }
